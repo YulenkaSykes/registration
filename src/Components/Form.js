47d8b14f246747b76f642604b76d59d615cc56b6
YulenkaSykes <72409591+YulenkaSykes@users.noreply.github.com>
2021-11-14 23:48:17 +0200

    fixed

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,5 +1,13 @@
 import React, { useState, useRef } from "react";
 import styles from "../styles/form.module.scss";
+import Country from "../country.png";
+import First from "../first.png";
+import Lock from "../lock.png";
+import Email from "../email.png";
+import Second from "../second.png";
+import Phone from "../phone.png";
+import TwoLock from "../twoLock.png";
+import Vector from "../Vector.png";
 
 import json from "../countryCodes.json";
 
@@ -17,8 +25,8 @@ const Form = () => {
     <div className={`col ${styles.wrapper}`}>
       <div className={`col form`}>
         <label className={`row label`}>
-          <div className={`icon`}>
-            <img src="/first.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={First} alt="" />
           </div>
           <input type="text" className={`input_text`} required />
           <span className={`floating_label`}>First Name</span>
@@ -28,8 +36,8 @@ const Form = () => {
           ref={selectRef}
           onClick={() => selectRef.current.focus()}
         >
-          <div className={`icon`}>
-            <img src="/country.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={Country} alt="" />
           </div>
           <Select
             options={countryCodes.map((country) => country.name)}
@@ -42,29 +50,29 @@ const Form = () => {
           </span>
         </label>
         <label className={`row label`}>
-          <div className={`icon`}>
-            <img src="/lock.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={Lock} alt="" />
           </div>
           <input type="text" className={`input_text`} required />
           <span className={`floating_label`}>Password</span>
         </label>
         <label className={`row label`}>
-          <div className={`icon`}>
-            <img src="/email.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={Email} alt="" />
           </div>
           <input type="text" className={`input_text`} required />
           <span className={`floating_label`}>Email</span>
         </label>
         <label className={`row label`}>
-          <div className={`icon`}>
-            <img src="/second.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={Second} alt="" />
           </div>
           <input type="text" className={`input_text`} required />
           <span className={`floating_label`}>Second name</span>
         </label>
         <label className={`row label`}>
-          <div className={`icon`}>
-            <img src="/phone.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={Phone} alt="" />
           </div>
           <input
             type="tel"
@@ -76,8 +84,8 @@ const Form = () => {
           <span className={`floating_label`}>Phone</span>
         </label>
         <label className={`row label`}>
-          <div className={`icon`}>
-            <img src="/twoLock.png" alt="" />
+          <div className={`row centered icon`}>
+            <img src={TwoLock} alt="" />
           </div>
           <input type="text" className={`input_text`} required />
           <span className={`floating_label`}>Confirm password</span>
@@ -88,7 +96,7 @@ const Form = () => {
             className={`row centered ${styles.checkbox}`}
             onClick={() => setChecked(!checked)}
           >
-            {checked && <img src="./Vector.png" alt="" />}
+            {checked && <img src={Vector} alt="" />}
           </div>
           <p className={styles.text}>
             I agree to the{" "}
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styles from "../styles/header.module.scss";
+import Img from "../logo.png";
 
 const Header = () => {
   return (
     <div className={`col centered ${styles.header}`}>
-      <img src="/logo.png" alt="" />
+      <img src={Img} alt="" />
       <div className={styles.text}>
         <h2>
           <span style={{ color: "#6CEEC7" }}>Sign Up</span> and find the best
